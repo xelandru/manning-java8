e59bc6c4e90764871938f3ef24b889508ba2d271
adascalu <alexandru.dascalu@adswizz.com>
2018-09-15 17:22:29 +0300

    added ToList Collector

--- src/main/java/chp11/Shop.java
+++ src/main/java/chp11/Shop.java
@@ -1,7 +1,29 @@
 package chp11;
 
-/**
- * Created by alex on 9/13/18.
- */
+
 public class Shop {
+
+    private String name;
+
+    public Shop(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double calculatePrice(String product) {
+        delay();
+        return Math.random()*product.charAt(0) + product.charAt(1);
+    }
+
+    private void delay() {
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
