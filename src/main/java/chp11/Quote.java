060db90581c5ccd8a096dd1b72455f128f08f645
adascalu <alexandru.dascalu@adswizz.com>
2018-09-16 20:56:16 +0300

    yup

---
+++ src/main/java/chp11/Quote.java
@@ -0,0 +1,7 @@
+package chp11;
+
+/**
+ * Created by lex on 9/16/18.
+ */
+public class Quote {
+}
---
+++ src/main/java/chp11/ShopPriceFinder.java
@@ -0,0 +1,7 @@
+package chp11;
+
+/**
+ * Created by lex on 9/15/18.
+ */
+public class ShopPriceFinder {
+}
